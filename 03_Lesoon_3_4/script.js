e7465bbc6caebcc6aaeba27f5375f0b9f4f3b7f6
Iryna Bilinska <88450867+Bilinska@users.noreply.github.com>
2022-12-21 17:24:12 +0100

    Data variables and types examples

---
+++ 03_Lesoon_3_4/script.js
@@ -0,0 +1,70 @@
+var message;
+message = "Hello";
+
+var mail;
+
+var age, firstName, lastname, email;
+
+age = 26;
+firstName = "Iryna";
+lastname = "Bilinka";
+email = "bilinska.iryna.02@gmail.com";
+
+console.log(message);
+console.log(mail);
+console.log(age);
+console.log(firstName);
+console.log(lastname);
+console.log(email);
+
+age = 27;
+age = 27 + 3;
+console.log(age);
+
+const city = "Lviv";
+console.log(city);
+// city = 'Warsaw';
+// console.log(city);
+
+let country = "Ukraine";
+console.log(country);
+
+country = "Poland";
+console.log(country);
+
+let dog, brother, mother, father;
+dog = "Africa";
+brother = "Vladyslav";
+mother = "Oksana";
+father = "Ihor";
+
+console.log(dog);
+console.log(brother);
+console.log(mother);
+console.log(father);
+
+dog = "Africa dog";
+brother = "Vladyslav Bilinskyi";
+mother = "Oksana Bilinska";
+father = "Ihor Bilinskyi";
+
+console.log(dog);
+console.log(brother);
+console.log(mother);
+console.log(father);
+
+if (true) {
+  var motto1 = "paragraph 1";
+  // let motto2 = "paragraph 2";
+  // const motto3 = "paragraph 3";
+}
+
+console.log(motto1);
+// console.log(motto2);
+// console.log(motto3);
+
+let RED_COLOR = "ff0000";
+console.log(RED_COLOR);
+
+let orangeColor = 'FFA500';
+console.log(orangeColor);
