d1f7912f1489e24050dc8170205b2f867abf357f
Iryna Bilinska <88450867+Bilinska@users.noreply.github.com>
2022-12-22 17:57:49 +0100

    Flow control operators' examples

--- 06_Lesson_7_8/script.js
+++ 06_Lesson_7_8/script.js
@@ -4,13 +4,13 @@ let x = 5,
   y = 10,
   z = 15;
 
-console.log(x + y);   // returns 15
-console.log(x - y);   // returns 5
-console.log(x * y);   // returns 50
-console.log(y / x);   // returns 2
-console.log(x % 2);   // returns 1
-console.log(x++);     // returns 6
-console.log(x--);     // returns 4
+console.log(x + y); // returns 15
+console.log(x - y); // returns 5
+console.log(x * y); // returns 50
+console.log(y / x); // returns 2
+console.log(x % 2); // returns 1
+console.log(x++);   // returns 6
+console.log(x--);   // returns 4
 
 var a = 5,
   b = 10,
@@ -33,17 +33,17 @@ console.log(a >= b);  // returns false
 
 console.log(a <= b);  // returns true
 
-console.log(a >= c); // returns true
+console.log(a >= c);  // returns true
 
-console.log(a <= b); // returns true
+console.log(a <= b);  // returns true
 
 var p = 5,
   o = 10;
 
 // NOT
-console.log("NOT\n\t", !(p < o));         // returns false
+console.log("NOT\n\t", !(p < o)); // returns false
 
-console.log("\t", !(p > o));              // returns true
+console.log("\t", !(p > o)); // returns true
 
 // AND
 console.log("AND\n\t", p != o && p < o);  // returns true
@@ -63,19 +63,21 @@ console.log("\t", false || true);         // returns true
 console.log("\t", false || false);        // returns false
 
 // Assignment operator
-var d = 5, b = 10, g = 15;
+var d = 5,
+  b = 10,
+  g = 15;
 
-console.log("Assignment\n\t", d = b);     // d === 10
+console.log("Assignment\n\t", (d = b));   // d === 10
 
-console.log("\t", d += 1);                // d === 11
+console.log("\t", (d += 1));              // d === 11
 
-console.log("\t", d -= 1);                // d === 10
+console.log("\t", (d -= 1));              // d === 10
 
-console.log("\t", d *= 5);                // d === 50
+console.log("\t", (d *= 5));              // d === 50
 
-console.log("\t", d /= b);                // d === 5
+console.log("\t", (d /= b));              // d === 5
 
-console.log("\t", d %= b);                // d === 5
+console.log("\t", (d %= b));              // d === 5
 
 console.log("\t", d + b * g);             // d === 155
 
